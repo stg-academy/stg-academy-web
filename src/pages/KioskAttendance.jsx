cf2660c48978b29377c68baa31c8c2067c011bb9
Yoonho Cho (Ray) <raycho521@yonsei.ac.kr>
2026-02-28 14:14:12 +0900

    [feat] 키오스크 ui 수정

--- src/pages/KioskAttendance.jsx
+++ src/pages/KioskAttendance.jsx
@@ -420,7 +420,7 @@ const KioskAttendance = () => {
 
       {/* 도움말 및 신규 회원 등록 */}
       <div className="bg-slate-100 p-4 flex-shrink-0">
-        <div className="max-w-2xl mx-auto flex items-center justify-between">
+        <div className="max-w-6xl px-6  mx-auto flex items-center justify-between">
           <div className="flex items-center text-sm text-slate-600">
             <svg className="w-4 h-4 mr-2 text-yellow-500" fill="currentColor" viewBox="0 0 20 20">
               <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd" />

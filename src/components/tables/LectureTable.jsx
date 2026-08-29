861857afd346a636433b7d766e301eb1a0d035cb
Yoonho Cho (Ray) <raycho521@yonsei.ac.kr>
2026-08-29 12:02:55 +0900

    [feat] 아이콘 개선, 디자인시스템 검토

--- src/components/tables/LectureTable.jsx
+++ src/components/tables/LectureTable.jsx
@@ -136,13 +136,13 @@ const LectureTable = ({
                     <div className="flex items-center space-x-2">
                         <button
                             onClick={() => onStartEdit && onStartEdit(row)}
-                            className="text-blue-600 hover:text-blue-700 text-sm font-medium"
+                            className="text-neutral-600 hover:text-neutral-900 text-sm font-medium"
                         >
                             <Edit/>
                         </button>
                         <button
                             onClick={() => onDeleteLecture && onDeleteLecture(row.id)}
-                            className="text-red-600 hover:text-red-700 text-sm font-medium"
+                            className="text-neutral-600 hover:text-neutral-900 text-sm font-medium"
                         >
                             <Delete/>
                         </button>
